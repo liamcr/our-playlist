a475ca9096cec9d726ba51fdf439f11f33317136
Liam Crocket <liamcrocket@gmail.com>
2018-08-31 16:03:37 -0400

    Delete folder

--- Desktop/OurPlaylist/scripts/roomNotFound.js
+++
@@ -1,6 +0,0 @@
-(function() {
-"use strict";
-    document.getElementById("back").addEventListener("click", function() {
-        window.location.href = "https://ourplaylist.000webhostapp.com";
-    });
-})();
