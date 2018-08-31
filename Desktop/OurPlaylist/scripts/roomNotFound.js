1079980841cf23130f8934a78c828bad899c72cd
Liam Crocket <liamcrocket@gmail.com>
2018-08-31 16:04:54 -0400

    Add js files

---
+++ Desktop/OurPlaylist/scripts/roomNotFound.js
@@ -0,0 +1,6 @@
+(function() {
+"use strict";
+    document.getElementById("back").addEventListener("click", function() {
+        window.location.href = "https://ourplaylist.000webhostapp.com";
+    });
+})();
